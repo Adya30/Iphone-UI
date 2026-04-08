07638bb90baf4ea748a703a8eda043075f5e23c4
adya <handikaadya@gmail.com>
2026-04-08 13:56:07 +0700

    final

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".menu-icon");
 const closeMenu = document.querySelector(".close-menu");
 const navLink = document.querySelector(".nav-link");
+const navItems = document.querySelectorAll(".nav-link a");
 
 menuIcon.addEventListener("click", () => {
     navLink.classList.add("active");
@@ -10,33 +11,73 @@ closeMenu.addEventListener("click", () => {
     navLink.classList.remove("active");
 });
 
-// iphone-hero
-const grid = document.getElementById('iphoneGrid');
-const btnLeft = document.getElementById('scrollLeft');
-const btnRight = document.getElementById('scrollRight');
-
-const scrollAmount = 200;
-
-btnRight.addEventListener('click', () => {
-    grid.scrollLeft += scrollAmount;
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navLink.classList.remove("active");
+    });
 });
 
-btnLeft.addEventListener('click', () => {
-    grid.scrollLeft -= scrollAmount;
+function setupScroll(gridId, btnLeftId, btnRightId, scrollAmount = 200) {
+    const grid = document.getElementById(gridId);
+    const btnLeft = document.getElementById(btnLeftId);
+    const btnRight = document.getElementById(btnRightId);
+
+    if (!grid || !btnLeft || !btnRight) return;
+
+    btnRight.addEventListener('click', () => {
+        grid.scrollLeft += scrollAmount;
+    });
+
+    btnLeft.addEventListener('click', () => {
+        grid.scrollLeft -= scrollAmount;
+    });
+
+    grid.addEventListener('scroll', () => {
+        if (grid.scrollLeft > 0) {
+            btnLeft.classList.add('visible');
+        } else {
+            btnLeft.classList.remove('visible');
+        }
+
+        if (grid.scrollLeft + grid.clientWidth >= grid.scrollWidth - 5) {
+            btnRight.classList.remove('visible');
+        } else {
+            btnRight.classList.add('visible');
+        }
+    });
+}
+
+
+// iphone hero
+setupScroll('iphoneGrid', 'scrollLeft', 'scrollRight');
+// explore
+setupScroll('exploreGrid', 'explorePrev', 'exploreNext');
+// gallery
+setupScroll('galleryGrid', 'galleryPrev', 'galleryNext');
+
+// iphone-menu-dropdowns
+const items = document.querySelectorAll(".accordion .item");
+
+items.forEach((item) => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    items.forEach(i => i.classList.remove("active"));
+
+    item.classList.add("active");
+  });
 });
 
-grid.addEventListener('scroll', () => {
-    if (grid.scrollLeft > 0) {
-        btnLeft.classList.add('visible');
-    } else {
-        btnLeft.classList.remove('visible');
+// footer-dropdown
+const footerTitles = document.querySelectorAll(".footer h4");
+
+footerTitles.forEach(title => {
+  title.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+
+      const parent = title.parentElement;
+
+      parent.classList.toggle("active");
     }
-
-    if (grid.scrollLeft + grid.clientWidth >= grid.scrollWidth - 5) {
-        btnRight.classList.remove('visible');
-    } else {
-        btnRight.classList.add('visible');
-    }
+  });
 });
-
-// section ip
